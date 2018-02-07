f3d0cac1f561e5088529cfcb1fbf633455b1af37
anbonald <anbonald@t49.torre.math.unipd.it>
2018-02-07 16:42:23 +0100

    cpp ++

--- Heuristics4Tsp/Heuristics4Tsp/TestBuilder.h
+++ Heuristics4Tsp/Heuristics4Tsp/TestBuilder.h
@@ -17,31 +17,4 @@ public:
 
 	static std::string outDir; 
 
-	void genereteTests()
-	{	
-	}
-
-	void generateGrid(double xSize, double ySize, int xFreq, int yFreq, double xoffset = 0.0, double yoffset = 0.0)
-	{
-		double xDistance = xSize / xFreq; 
-		double yDistance = ySize / yFreq;
-
-		int xNUM = (int)xSize / xDistance; 
-		int yNUM = (int)ySize / yDistance;
-
-		std::vector<Point> grid; 
-		grid.reserve(xNUM*yNUM); 
-
-		double x = 0.0 + xoffset;
-		double y = 0.0 + yoffset;
-
-		for (int i = 0; i < xNUM; i++)
-		{
-			for (int j = 0; j < yNUM; j++)
-			{
-				grid.at(i*xNUM + j) = Point(x + xNUM*xDistance, y + yNUM*yDistance); 
-			}
-		}
-
-	}
 };
